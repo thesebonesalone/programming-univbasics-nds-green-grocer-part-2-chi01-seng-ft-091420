650748351979897889a0e6c2059a2c92e102a772
Willem A Duiker <duikhead2@gmail.com>
2020-08-25 17:03:49 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,6 +4,24 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart.each do |cart_item|
+    coupons.each do |coupon_item|
+      if cart_item[:item] == coupon_item[:item]
+        if cart_item[:count] >= coupon_item[:num]
+          new_price = coupon_item[:cost]/coupon_item[:num]
+          new_name = "#{coupon_item[:item]} W/COUPON"
+          new_clearance = cart_item[:clearance]
+          new_count = cart_item[:count] - (cart_item[:count] % coupon_item[:num])
+          cart_item[:count] = (cart_item[:count] % coupon_item[:num])
+          cart.push({:item => new_name, :price => new_price, :clearance => new_clearance, :count => new_count})
+        end
+      end
+    end
+  end
+  return cart
+          
+  
+  
 end
 
 def apply_clearance(cart)
